05cf4ac83ec8952395a9ce185c57787538b87e33
JessicaHerford <jrherford12@gmail.com>
2016-05-20 16:08:40 -0600

    working on refactor

--- testfile.js
+++ testfile.js
@@ -4,6 +4,19 @@ var driver = new webdriver.Builder().
     withCapabilities(webdriver.Capabilities.chrome()).
     build();
 
-driver.get('http://www.wingify.com').then(function(){
-  // driver.click('class=button.filled-shadow.blue');
+// driver.get('http://www.wingify.com')
+// .then(function(){
+//   driver.click('button.filled-shadow.blue');
+// });
+
+
+describe('basic test', function () {
+	it('should be on correct page', function () {
+		driver.get('http://www.wingify.com');
+		// driver.getTitle().then(function(title) {
+		// 	expect(title).toBe('Wingify');
+    //   done();
+		// });
+    expect(driver.getTitle()).toBe('Wingify');
+	});
 });
